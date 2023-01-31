047f4f4ca191fc546241eef551681a02922e204e
wgota <zegotaaofc@gmail.com>
2023-01-31 01:57:04 -0300

    main

--- app.js
+++ app.js
@@ -22,4 +22,11 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
+    console.log('Server running on port 3000');
+    console.log('Server running on port 3000');
+    console.log('Server running on port 3000');
+    console.log('Server running on port 3000');
+    console.log('Server running on port 3000');
+    console.log('Server running on port 3000');
+    console.log('Server running on port 3000');
 });
