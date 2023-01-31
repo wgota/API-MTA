1a9463f334c956c7fc79997c7c74569f320e368c
wgota <zegotaaofc@gmail.com>
2023-01-31 01:05:33 -0300

    inicial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const gamedig = require('gamedig');
+
+const app = express();
+
+app.get('/server/:ip/:port', (req, res) => {
+    const { ip, port } = req.params;
+    gamedig.query({
+        type: 'mtasa',
+        host: ip,
+        port: port
+    }).then((state) => {
+        res.json(state);
+    }).catch((error) => {
+        res.status(500).json({error: error.message});
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
